c153241da08623f19895f2bfac4285226df637f2
Andrew Ezeani <ezeaniiandrew@gmail.com>
2023-08-18 00:53:23 +0100

    This commit was made from a phone

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+const [count, setCount] = useState(0)
+  return (
+    <>
+      <p className="read-the-docs">
+        hello world
+      </p>
+    </>
+  )
+}
+
+export default App
